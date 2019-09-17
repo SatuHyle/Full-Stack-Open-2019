3550fed23b6e84c61c0174673994675dd8034027
Satu Hyle <shyle90@gmail.com>
2019-09-17 11:04:36 +0300

    Update index.js

--- osa1/kurssitiedot_step1/src/index.js
+++ osa1/kurssitiedot_step1/src/index.js
@@ -10,6 +10,18 @@ const Header = (props) => {
     )
   }
 
+const Content = (props) => {
+    return (
+      <div>
+        <h2>{props.part1}</h2>
+        <p>Number of exercises: {props.exercises1}</p>
+        <h2>{props.part2}</h2>
+        <p>Number of exercises: {props.exercises2}</p>
+        <h2>{props.part3}</h2>
+        <p>Number of exercises: {props.exercises3}</p>
+      </div>
+    )
+  }
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -22,6 +34,9 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
+      <Content part1={part1} exercises1={exercises1} part2={part2}
+      exercises2={exercises2} part3={part3} exercises3={exercises3}
+      />
     </div>
   )
 }
